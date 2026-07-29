d2859142bde6f8b3287752a10132dbf72c04e549
Paul Serna-Torre <psernatorre@gmail.com>
2026-07-28 19:11:40 -0700

    feat: tuning formulas for inner current controller and inner voltage controller.

--- tests/pll_tuning.py
+++ tests/pll_tuning.py
@@ -23,9 +23,70 @@ def tune_pll(w_n: float,
     # Calculate integral gain
     ki = w_n**2
 
-    print(f"PLL controller Gains: kp = {kp:.4f}, ki = {ki:.4f}")
+    print(f"Tuned gains of the PLL: kp [rad/s] = {kp:.4f}, ki [rad^2/s^2] = {ki:.4f}")
 
     return {
         "kp": kp,
         "ki": ki
     }
+
+def tune_inner_current_controller(wcc: float, rf: float, xf: float, wnom: float) -> tuple:
+	"""
+	It tunes the inner current control loop based on the parameters of the filter.
+	
+	Inputs:
+	- wcc [rad/s]: bandwidth of the inner current controller. It is the inverse of the time constant 
+                   of the inner current controller. Values like 10000 rad/s up to 20000 rad/s can be used.
+                   The idea is that the current controller should be faster.
+                   This means that if the reference current is a step, the controlled current in the filter 
+                   should almost match the reference.
+	- rf [pu]: resistance of the filter [pu]. For example, the current controller
+               is used in a GFL where the filter is an LCL filter, then rf is the resistance 
+               of the first branch and second branch rf = rf1 + rf2
+               However, if the current controller is used in a GFM that has current controller
+               and voltage controller in cascade, then rf = rf1 because the current controller
+               is only controlling the current in the first branch of the LCL filter.
+	- xf [pu]: reactance of the filter [pu]. As the resistance, it is the 
+               sum of the reactances of the first and second branches of the LCL filter xf = xf1 + xf2.
+               In the case of a GFM, xf = xf1 because the current controller is only controlling 
+               the current in the first branch of the LCL filter.
+	- wnom [rad/s]: nominal frequency [rad/s]. It is 2 * pi * f_base, where f_base is the base frequency of the system in Hz.
+
+	Outputs:
+	- kp_pu [pu]: proportional gain of the inner current controller
+	- ki_puHz [pu/s]: integral gain of the inner current controller
+	"""
+     
+	# Compute kp and ki of the inner current control loop based on tuning formulas
+	tau = 1 / wcc # time constant of the inner current controller [s]
+	kp_cc_pu = xf * wcc / wnom # proportional gain of the inner current controller [pu]
+	ki_cc_puHz = rf * wcc # integral gain of the inner current controller [pu/s]
+
+	print(f'Tuned gains of the inner current controller: kp [pu]: {kp_cc_pu}, ki [pu/s]: {ki_cc_puHz}, tau [s]: {tau}')
+     
+	return { "kp": kp_cc_pu, "ki": ki_cc_puHz }
+
+def tune_inner_voltage_controller(wcc: float, phase_margin: float, cf: float, wnom: float) -> tuple:
+    """
+    It tunes the inner voltage control loop based on the parameters of the filter.
+
+    Inputs:
+    - wcc [rad/s]: bandwidth of the inner voltage controller. Similar to the bandwith of the inner current controller.
+    - phase_margin [rad]: phase margin of the inner voltage controller. It should be between 30 and 75 degrees.
+    - cf [pu]: capacitance of the filter's sunt in per unit.
+    - wnom [rad/s]: nominal frequency [rad/s]. It is 2 * pi * f_base, where f_base is the base frequency of the system in Hz.
+
+    Outputs:
+    - kp [pu]: proportional gain of the inner voltage controller
+    - ki [pu/s]: integral gain of the inner voltage controller
+    """
+     
+    z = wcc * (1 - np.sin(phase_margin)) / (1 + np.sin(phase_margin))
+    wm = np.sqrt(wcc * z)
+
+    kp = (cf * wm) / wnom
+    ki = (cf * wm**3) / (wnom * wcc)
+
+    print(f'Tuned parameters for voltage controller - kp [pu]: {kp}, ki [pu/s]: {ki}')
+
+    return { "kp": kp, "ki": ki }
